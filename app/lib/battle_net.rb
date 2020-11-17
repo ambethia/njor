f26164c03448b8a57808b32fc19d9ef4f98825ca
Jason L Perry <jason@suncoast.io>
2020-11-17 16:13:26 -0500

    Update battle_net.rb

--- app/lib/battle_net.rb
+++ app/lib/battle_net.rb
@@ -1,14 +1,13 @@
-require 'net/http'
+require "net/http"
 
 module BattleNet
-
   def self.access_token
     client_credentials = Setting["client_credentials"]
     unless client_credentials && Time.parse(client_credentials["expires_at"]).future?
       request = Faraday.new("https://us.battle.net") do |conn|
-        conn.basic_auth(ENV['BNET_CLIENT_ID'], ENV['BNET_CLIENT_SECRET'])
+        conn.basic_auth(ENV["BNET_CLIENT_ID"], ENV["BNET_CLIENT_SECRET"])
       end
-      data = JSON.parse(request.post("/oauth/token", "grant_type=client_credentials"))
+      data = JSON.parse(request.post("/oauth/token", "grant_type=client_credentials").body)
       data["expires_at"] = data["expires_in"].seconds.from_now
       Setting["client_credentials"] = data
       data["access_token"]
@@ -17,10 +16,11 @@ module BattleNet
     end
   end
 
-  def self.get(path, namespace = "dynamic-us")
+  def self.get(path_or_uri, namespace = "static-us")
+    uri = URI(path_or_uri)
     request = Faraday.new("https://us.api.blizzard.com") do |conn|
       conn.authorization :Bearer, access_token
     end
-    JSON.parse(request.get(path, namespace: namespace, locale: 'en_US').body)
+    JSON.parse(request.get(uri.path, namespace: namespace, locale: "en_US").body)
   end
 end
