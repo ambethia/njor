40d3858075485ebeaa592bef0d1baeb15e47315c
Jason L Perry <jason@suncoast.io>
2020-11-16 21:10:32 -0500

    Remove realms and test stuff

--- config/environments/test.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.configure do
-  config.consider_all_requests_local = true
-  config.cache_classes = true
-  config.eager_load = false
-  config.action_dispatch.show_exceptions = true
-  config.active_support.deprecation = :stderr
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_032450) do
+ActiveRecord::Schema.define(version: 2020_05_27_190825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "realms", force: :cascade do |t|
-    t.string "name"
-    t.string "slug"
-    t.integer "connected_realm"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "settings", force: :cascade do |t|
     t.string "name"
