1132aacbfa8f3f50bcc71b019f9a8ec42824d281
Jason L Perry <jason@suncoast.io>
2020-11-17 16:13:58 -0500

    Magic controllers (lol?)

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,18 @@
 Rails.application.routes.draw do
+  with_options only: [:index, :show] do |read_only|
+    read_only.resources :playable_classes do
+      read_only.resources :specializations
+    end
+  end
+
+  resources :keymaps do
+    resources :action_bars do
+      resources :action_buttons do
+        resources :keybind
+      end
+    end
+  end
+
+  resources :categories
+  resources :abilities
 end
